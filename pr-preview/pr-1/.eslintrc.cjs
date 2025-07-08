dfdacf4eb87e2b63277c6eaa4b86611b4ef789b5
valphy <valphy@users.noreply.github.com>
2025-07-08 11:11:28 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/.eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  "env": { "browser": true, "es2021": true },
+  "extends": ["eslint:recommended", "plugin:import/recommended", "prettier"],
+  "parserOptions": { "ecmaVersion": 12, "sourceType": "module" },
+  "rules": {}
+};
